07b417f71049ed43d8aea2d1447e20b44b581939
Daniel Ochoa <ing.danielochoa@hotmail.com>
2023-07-09 21:47:44 -0500

    Entrega 3

---
+++ js/productos.js
@@ -0,0 +1,108 @@
+export class Producto{
+    constructor(productoRecibidoPorParametro){
+        const { id , precio , nombre, promo, precioPromo, recomendado,img} = productoRecibidoPorParametro;
+       this.id = id;
+       this.precio = precio;
+       this.nombre = nombre;
+       this.promo  = promo;
+       this.precioPromo = precioPromo;
+       this.recomendado = recomendado;
+       this.img = img;
+
+    }
+}
+
+export function inicializarProductos(arrayDeProductos) {
+    arrayDeProductos.push(new Producto({
+        id:arrayDeProductos.length +1,
+        precio:284.361,
+        nombre:"ADIDAS NEBZED CLOUDFOAM",
+        promo:false,
+        precioPromo:284.361,
+        recomendado:false,
+        img:"./assets/img/tenis1.avif"
+    }));
+
+    arrayDeProductos.push(new Producto({
+        id:arrayDeProductos.length +1,
+        precio:459.952,
+        nombre:"ADIDAS TOP TEN RB",
+        promo:true,
+        precioPromo:271.371,
+        recomendado:true,
+        img:"./assets/img/tenis2.avif"
+    }));
+
+    arrayDeProductos.push(new Producto({
+        id:arrayDeProductos.length +1,
+        precio:379.952,
+        nombre:"ADIDAS EQ19 RUN",
+        promo:true,
+        precioPromo:279.955,
+        recomendado:false,
+        img:"./assets/img/tenis3.avif"
+    }));
+
+    arrayDeProductos.push(new Producto({
+        id:arrayDeProductos.length +1,
+        precio:329.952,
+        nombre:"ADIDAS QT RACER 3.0",
+        promo:true,
+        precioPromo:194.671,
+        recomendado:true,
+        img:"./assets/img/tenis4.avif"
+    }));
+
+    arrayDeProductos.push(new Producto({
+        id:arrayDeProductos.length +1,
+        precio:594.999,
+        nombre:"Nike Court Vision",
+        promo:true,
+        precioPromo:475.992,    
+        recomendado:false,
+        img:"./assets/img/tenis5.jfif"
+    }));
+
+    arrayDeProductos.push(new Producto({
+        id:arrayDeProductos.length +1,
+        precio:569.999,
+        nombre:"Running Renew Run 3",
+        promo:false,
+        precioPromo:569.999,    
+        recomendado:false,
+        img:"./assets/img/tenis6.jfif"
+    }));
+
+    arrayDeProductos.push(new Producto({
+        id:arrayDeProductos.length +1,
+        precio:619.999,
+        nombre:"Jordan Series Es",
+        promo:true,
+        precioPromo:495.992,    
+        recomendado:true,
+        img:"./assets/img/tenis7.jfif"
+    }));
+
+    arrayDeProductos.push(new Producto({
+        id:arrayDeProductos.length +1,
+        precio:919.999,
+        nombre:"Air Max 90",
+        promo:true,
+        precioPromo:735.992,    
+        recomendado:true,
+        img:"./assets/img/tenis8.jfif"
+    }));
+}
+
+export const agregarAlCarrito = (carrito, producto) => {
+    carrito.push(producto)
+}
+
+export const eliminarDelCarrito = (carrito, producto) => {
+    carrito = carrito.filter(prod => prod.id != producto.id);
+    localStorage.removeItem("carrito");
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+}
+
+
+export const CANTIDAD_MAXIMA_DEL_CARRITO = 10;
---
+++ js/scripts.js
@@ -0,0 +1,300 @@
+import { inicializarProductos, agregarAlCarrito, eliminarDelCarrito } from './productos.js'
+
+const arrayDeProductos = [];
+let arrayCarrito = JSON.parse(localStorage.getItem("carrito")) || [];
+
+inicializarProductos(arrayDeProductos);
+
+const productos = document.querySelector("#productos");
+const countItemsCarrito = document.querySelector("#countItemsCarrito");
+countItemsCarrito.innerText = arrayCarrito.length;
+const carritoButton = document.querySelector("#carritoButton");
+const paymentButton = document.querySelector("#paymentButton");
+
+arrayDeProductos.forEach((prod) => {
+    const card = document.createElement("div");
+    card.classList.add("col");
+    card.classList.add("mb-5");
+    let cardItem = "";
+    if (!prod.promo) {
+        cardItem = `
+            <div class="card h-100">
+                <!-- Product image-->
+                <img class="card-img-top" src="${prod.img}" alt="..." />
+                <!-- Product details-->
+                <div class="card-body p-4">
+                    <div class="text-center">
+                        <!-- Product name-->
+                        <h5 class="fw-bolder">${prod.nombre}</h5>
+                        <!-- Product price-->
+                        $${prod.precio}
+                    </div>
+                </div>
+                <!-- Product actions-->
+                <div class="card-footer p-4 pt-0 border-top-0 bg-transparent text-center">
+                <button id="button-${prod.id}" type="button" class="btn btn-outline-dark mt-auto">
+                    Agregar al carrito
+                </button>
+                </div>
+            </div>
+        `;
+    } else if (prod.promo && prod.recomendado) {
+        cardItem = `
+        <div class="card h-100">
+            <!-- Sale badge-->
+            <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Promo
+            </div>
+            <!-- Product image-->
+            <img class="card-img-top" src="${prod.img}" alt="..." />
+            <!-- Product details-->
+            <div class="card-body p-4">
+                <div class="text-center">
+                    <!-- Product name-->
+                    <h5 class="fw-bolder">${prod.nombre}</h5>
+                    <!-- Product reviews-->
+                    <div class="d-flex justify-content-center small text-warning mb-2">
+                        <div class="bi-star-fill"></div>
+                        <div class="bi-star-fill"></div>
+                        <div class="bi-star-fill"></div>
+                        <div class="bi-star-fill"></div>
+                        <div class="bi-star-fill"></div>
+                    </div>
+                    <!-- Product price-->
+                    <span class="text-muted text-decoration-line-through">$${prod.precio}</span>
+                    $${prod.precioPromo}
+                </div>
+            </div>
+            <!-- Product actions-->
+            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent text-center">
+            <button id="button-${prod.id}" type="button" class="btn btn-outline-dark mt-auto">
+                 Agregar al carrito
+            </button>
+            </div>
+        </div>
+    `
+    } else if (prod.promo && !prod.recomendado) {
+        cardItem = `
+        <div class="card h-100">
+            <!-- Sale badge-->
+            <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Promo
+            </div>
+            <!-- Product image-->
+            <img class="card-img-top" src="${prod.img}" alt="..." />
+            <!-- Product details-->
+            <div class="card-body p-4">
+                <div class="text-center">
+                    <!-- Product name-->
+                    <h5 class="fw-bolder">${prod.nombre}</h5>
+                    <!-- Product price-->
+                    <span class="text-muted text-decoration-line-through">$${prod.precio}</span>
+                    $${prod.precioPromo}
+                </div>
+            </div>
+            <!-- Product actions-->
+            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent text-center">
+                <button id="button-${prod.id}" type="button" class="btn btn-outline-dark mt-auto">
+                        Agregar al carrito
+                </button>
+            </div>
+        </div>
+    `
+    }
+
+    card.innerHTML = cardItem;
+    productos.appendChild(card);
+
+    const buttonAgregar = document.querySelector("#button-" + prod.id);
+
+    buttonAgregar.addEventListener("click", () => {
+        agregarAlCarrito(arrayCarrito, prod);
+        localStorage.setItem("carrito", JSON.stringify(arrayCarrito));
+        Swal.fire({
+            position: 'center',
+            icon: 'success',
+            title: 'Se agrego al carrito el producto ' + prod.nombre,
+            showConfirmButton: true,
+            confirmButtonText: 'Ok',
+            timer: 3000
+        })
+        arrayCarrito = JSON.parse(localStorage.getItem("carrito")) || [];
+        countItemsCarrito.innerText = arrayCarrito.length;
+    })
+
+})
+
+carritoButton.addEventListener("click", () => {
+    recalcularCarrito();
+    countItemsCarrito.innerText = arrayCarrito.length;
+    if(arrayCarrito.length == 0){
+        const listaCarrito = document.querySelector("#listaCarrito");
+        listaCarrito.innerHTML =` <div class="text-center lead fw-normal text-black-50 mb-0">
+        ¡Tu carrito esta vacío!
+        <div class="slide-header">
+         <i class="bi bi-cart-x-fill img-cart-empty"></i>
+        </div>`;
+    }
+})
+
+function recalcularCarrito() {
+    const listaCarrito = document.querySelector("#listaCarrito");
+    const cantArt = document.querySelector("#cantArt");
+    const subtotal = document.querySelector("#subtotal");
+    const ivaPago = document.querySelector("#iva");
+    const total = document.querySelector("#total");
+
+    listaCarrito.innerHTML = '';
+    arrayCarrito.forEach((prod) => {
+        let card = document.createElement("div");
+        let cardItem = "";
+        if (!prod.promo) {
+            cardItem = `
+                <div class="card h-100">
+                    <!-- Product image-->
+                    <img class="card-img-top" src="${prod.img}" alt="..." />
+                    <!-- Product details-->
+                    <div class="card-body p-4">
+                        <div class="text-center">
+                            <!-- Product name-->
+                            <h5 class="fw-bolder">${prod.nombre}</h5>
+                            <!-- Product price-->
+                            $${prod.precio}
+                        </div>
+                    </div>
+                    <!-- Product actions-->
+                    <div class="card-footer p-4 pt-0 border-top-0 bg-transparent text-center">
+                    <button id="buttonModal-${prod.id}" type="button" class="btn btn-outline-dark mt-auto">
+                        Eliminar del carrito
+                    </button>
+                    </div>
+                </div>
+            `;
+        } else if (prod.promo && prod.recomendado) {
+            cardItem = `
+            <div class="card h-100">
+                <!-- Sale badge-->
+                <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Promo
+                </div>
+                <!-- Product image-->
+                <img class="card-img-top" src="${prod.img}" alt="..." />
+                <!-- Product details-->
+                <div class="card-body p-4">
+                    <div class="text-center">
+                        <!-- Product name-->
+                        <h5 class="fw-bolder">${prod.nombre}</h5>
+                        <!-- Product reviews-->
+                        <div class="d-flex justify-content-center small text-warning mb-2">
+                            <div class="bi-star-fill"></div>
+                            <div class="bi-star-fill"></div>
+                            <div class="bi-star-fill"></div>
+                            <div class="bi-star-fill"></div>
+                            <div class="bi-star-fill"></div>
+                        </div>
+                        <!-- Product price-->
+                        <span class="text-muted text-decoration-line-through">$${prod.precio}</span>
+                        $${prod.precioPromo}
+                    </div>
+                </div>
+                <!-- Product actions-->
+                <div class="card-footer p-4 pt-0 border-top-0 bg-transparent text-center">
+                <button id="buttonModal-${prod.id}" type="button" class="btn btn-outline-dark mt-auto">
+                    Eliminar del carrito
+                </button>
+                </div>
+            </div>
+        `
+        } else if (prod.promo && !prod.recomendado) {
+            cardItem = `
+            <div class="card h-100">
+                <!-- Sale badge-->
+                <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Promo
+                </div>
+                <!-- Product image-->
+                <img class="card-img-top" src="${prod.img}" alt="..." />
+                <!-- Product details-->
+                <div class="card-body p-4">
+                    <div class="text-center">
+                        <!-- Product name-->
+                        <h5 class="fw-bolder">${prod.nombre}</h5>
+                        <!-- Product price-->
+                        <span class="text-muted text-decoration-line-through">$${prod.precio}</span>
+                        $${prod.precioPromo}
+                    </div>
+                </div>
+                <!-- Product actions-->
+                <div class="card-footer p-4 pt-0 border-top-0 bg-transparent text-center">
+                    <button id="buttonModal-${prod.id}" type="button" class="btn btn-outline-dark mt-auto">
+                        Eliminar del carrito
+                    </button>
+                </div>
+            </div>
+        `
+        }
+
+        card.innerHTML = cardItem;
+        listaCarrito.appendChild(card);
+
+        const buttonEliminar = document.querySelector("#buttonModal-" + prod.id);
+
+        buttonEliminar.addEventListener("click", () => {
+            Swal.fire({
+                title: 'Estas seguro?',
+                text: "Se eliminara este producto del carrito!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Si, eliminar!'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    eliminarDelCarrito(arrayCarrito, prod);
+                    arrayCarrito = JSON.parse(localStorage.getItem("carrito")) || [];
+                    countItemsCarrito.innerText = arrayCarrito.length;
+                    recalcularCarrito();
+                    Swal.fire(
+                        'Eliminado!',
+                        'El producto ha sido eliminado.',
+                        'success'
+                    )
+                }
+            })
+        })
+
+    });
+
+    let monto = parseFloat((arrayCarrito.reduce((acumulador, prod) => acumulador + prod.precio, 0)).toFixed(3));
+    let procentajeIva = 18;
+    let iva = parseFloat(((monto * procentajeIva)/100).toFixed(3));
+    let valorTotal =(monto+iva).toFixed(3);
+
+    cantArt.value=arrayCarrito.length;
+    subtotal.value=monto;
+    ivaPago.value=iva;
+    total.value=valorTotal;
+
+}
+
+
+paymentButton.addEventListener("click", () => {
+
+    Swal.fire({
+        title: 'Estas seguro?',
+        text: "Realizaras el pago de los productos del carrito!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Si, pagar!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            Swal.fire(
+                'Pago Exitoso!',
+                'Muchas gracias por tu compra, tu pago ha sido exitoso con N° de pedido 24234.',
+                'success'
+            )
+            localStorage.removeItem("carrito");
+            setTimeout(function(){
+                location.reload()
+            }, 5000);
+        }
+    })
+})
